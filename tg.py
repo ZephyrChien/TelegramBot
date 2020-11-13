a002ad85426e3cc84a0a6ecd064cf20381556414
zephyr <i@zephyr.moe>
2020-11-13 21:29:48 +0900

    add timer

--- tg.py
+++ tg.py
@@ -6,9 +6,13 @@ import utils
 import config
 import payment
 import telebot
+import threading
 
-bot = telebot.TeleBot(config.TOKEN)
 app = flask.Flask(__name__)
+bot = telebot.TeleBot(config.TOKEN)
+timer = utils.Timer()
+timer_thread = threading.Thread(target=timer.start,daemon=True)
+timer_thread.start()
 
 #cmd list
 @bot.message_handler(commands=['help'])
@@ -20,7 +24,7 @@ def cmd_help(message):
 @utils.send_to_me
 def cmd_start(message):
     bot.send_message(message.chat.id,'Ciallo')
-    bot.send_message(message.chat.id,'With this bot, you can:')
+    bot.send_message(message.chat.id,'with this bot, you can:')
     bot.send_message(message.chat.id,config.USAGE)
 
 @bot.message_handler(commands=['sub'])
@@ -36,23 +40,27 @@ def cmd_py(message):
     upw = config.UPW
     cmd_args=message.text.split(' ')
     if len(cmd_args) != 3:
-        bot.send_message(message.chat.id,'Plz follow this format: /py alipay(or wxpay) 5')
+        bot.send_message(message.chat.id,'plz follow this format: /py alipay(or wxpay) 5')
         return
     elif cmd_args[1] not in ['alipay','wxpay']:
-        bot.send_message(message.chat.id,'Only support alipay or wxpay')
+        bot.send_message(message.chat.id,'only support alipay or wxpay')
         return
     else:
         try:
             amount = int(cmd_args[2])
         except ValueError:
-            bot.send_message(message.chat.id,'Amount should be an unsigned integer')
+            bot.send_message(message.chat.id,'amount should be an unsigned integer')
             return
         else:
             if amount <= 0:
-                bot.send_message(message.chat.id,'Amount should be an unsigned integer')
+                bot.send_message(message.chat.id,'amount should be an unsigned integer')
                 return
     bot.send_message(message.chat.id,'PYing...')
     bot.send_message(message.chat.id,'method: '+ cmd_args[1] + '\n' + 'amount: ' + cmd_args[2])
+    
+    if not timer.checkAndReset(message.chat.id,30,1):
+        bot.send_message(message.chat.id,'too much request \nplz wait ' + str(timer.dict[message.chat.id][0]) + 's')
+        return
     
     bill = payment.Bill(uid,upw)
     bill.initCharge(cmd_args[1],cmd_args[2])
@@ -60,13 +68,14 @@ def cmd_py(message):
     if not ok:
         bot.send_message(message.chat.id,'error')
         return
-    bot.send_message(message.chat.id,bill_qrcode)
-    bot.send_message(message.chat.id,'Plz complete this transaction in 30 minutes')
+    bot.send_message(message.chat.id,bill_id)
+    bot.send_photo(message.chat.id,bill_qrcode)
+    bot.send_message(message.chat.id,'plz complete this transaction in 30 minutes')
     time.sleep(20)
     for _ in range(0,3):
         ok = bill.verify(bill_id)
         if ok:
-            bot.send_message(message.chat.id,'Finished. Thanks a lot!')
+            bot.send_message(message.chat.id,'Finished. Thank you!')
             break
         time.sleep(5)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding:utf-8
+import time
 import config
 import telebot
 from functools import wraps
@@ -18,5 +19,41 @@ def send_to_me(func):
         return func(message)
     return cmd
 
+class Timer():
+    def __init__(self):
+        self.dict = {}
+    
+    def set(self, id, cd, times=0):
+        self.dict[id] = [cd, times]
+        
+    def cancel(self, id):
+        del self.dict[id]
+
+    def start(self):
+        del_list = []
+        while True:
+            for _ in range(len(del_list)):
+                id = del_list.pop(0)
+                del self.dict[id]
+
+            for id, lmt in self.dict.items():
+                if not lmt[0]:
+                    del_list.append(id)
+                    continue
+                lmt[0] -= 1
+            time.sleep(1)
+
+    def checkAndReset(self, id, cd, times=0):
+        if not self.dict.get(id):
+            self.set(id,cd,times)
+            return True
+        else:
+            if self.dict[id][1]:
+                self.set(id,cd,self.dict[id][1]-1)
+                return True
+            else:
+                return False
+            
+                
 if __name__ == "__main__":
     pass
